971ffdb13e0016d80141aff23ace4ec83fc60671
Hyeonki Hong <hhk7734@gmail.com>
2020-05-16 02:53:36 +0900

    example: update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:react_grid_view/react_grid_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,6 +22,105 @@ class Test extends StatefulWidget {
 class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ReactGridView(
+        crossAxisCount: 3,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        gridAspectRatio: 0.5,
+        children: [
+          ReactPositioned(
+            crossAxisOffsetCellCount: 0,
+            mainAxisOffsetCellCount: 0,
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Container(
+              color: Colors.red[100],
+            ),
+          ),
+          ReactPositioned(
+            crossAxisOffsetCellCount: 1,
+            mainAxisOffsetCellCount: 0,
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Container(
+              color: Colors.red[200],
+            ),
+          ),
+          ReactPositioned(
+            crossAxisOffsetCellCount: 2,
+            mainAxisOffsetCellCount: 0,
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Container(
+              color: Colors.red[300],
+            ),
+          ),
+          ReactPositioned(
+            crossAxisOffsetCellCount: 0,
+            mainAxisOffsetCellCount: 1,
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Container(
+              color: Colors.red[400],
+            ),
+          ),
+          ReactPositioned(
+            crossAxisOffsetCellCount: 1,
+            mainAxisOffsetCellCount: 1,
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Container(
+              color: Colors.red[500],
+            ),
+          ),
+          ReactPositioned(
+            crossAxisOffsetCellCount: 2,
+            mainAxisOffsetCellCount: 1,
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Container(
+              color: Colors.red[100],
+            ),
+          ),
+          ReactPositioned(
+            crossAxisOffsetCellCount: 0,
+            mainAxisOffsetCellCount: 2,
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Container(
+              color: Colors.red[200],
+            ),
+          ),
+          ReactPositioned(
+            crossAxisOffsetCellCount: 1,
+            mainAxisOffsetCellCount: 2,
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Container(
+              color: Colors.red[300],
+            ),
+          ),
+          ReactPositioned(
+            crossAxisOffsetCellCount: 2,
+            mainAxisOffsetCellCount: 2,
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Container(
+              color: Colors.red[400],
+            ),
+          ),
+          ReactPositioned(
+            crossAxisOffsetCellCount: 0,
+            mainAxisOffsetCellCount: 3,
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: Container(
+              color: Colors.red[500],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

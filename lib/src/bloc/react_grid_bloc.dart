972d3152736e6c537d5ded1b6a8ee4efc6edc85b
Hyeonki Hong <hhk7734@gmail.com>
2020-11-16 18:07:31 +0900

    bloc: add layout parameter

--- lib/src/bloc/react_grid_bloc.dart
+++ lib/src/bloc/react_grid_bloc.dart
@@ -7,7 +7,23 @@ part 'react_grid_event.dart';
 part 'react_grid_state.dart';
 
 class ReactGridBloc extends Bloc<ReactGridEvent, ReactGridState> {
-  ReactGridBloc() : super(ReactGridInitial());
+  int crossAxisCount;
+  double crossAxisSpacing;
+  double crossAxisStride;
+  double gridAspectRatio;
+  int mainAxisCount;
+  double mainAxisSpacing;
+  double mainAxisStride;
+
+  ReactGridBloc({
+    @required this.crossAxisCount,
+    this.crossAxisSpacing = 0.0,
+    @required this.crossAxisStride,
+    @required this.gridAspectRatio,
+    @required this.mainAxisCount,
+    this.mainAxisSpacing = 0.0,
+    @required this.mainAxisStride,
+  }) : super(ReactGridInitial());
 
   @override
   Stream<ReactGridState> mapEventToState(
